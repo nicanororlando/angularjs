ac9193d388a50af0f978ce4db90a2f1d46b0e4ab
nicanor <nicanororlando@gmail.com>
2022-08-25 23:14:39 -0300

    agrego proyectos

--- creencias-adventistas/app/app.module.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-
-// Define the `app` module
-angular.module("app", ["ngAnimate", "ngRoute", "core", "view", "view2"]);
--- creencias-adventistas/app/components/view2/view2.module.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-
-// Define the `phoneList` module
-angular.module("view2", ["ngRoute", "core.phone"]);
